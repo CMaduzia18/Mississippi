38c29b1a1b570d21a3b5793e94a92a427cc6d0af
CMaduzia18 <windward_student@school.windwardschool.org>
2016-09-06 08:52:40 -0700

    Mississippi Homework

---
+++ src/Mississippi.java
@@ -0,0 +1,57 @@
+
+public class Mississippi {
+	public static void m(){
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println();
+	}
+	public static void i(){
+		System.out.println("  IIIII");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("  IIIII");
+		System.out.println();
+	}
+	public static void s(){
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println();
+	}
+	public static void p(){
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println();
+	}
+	public static void main(String[] args){
+		m();
+		i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();
+	}
+}
